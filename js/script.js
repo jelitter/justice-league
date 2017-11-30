2621f0b206a555c3c32e05555e42360f2e1e2cc1
Isaac Sanchez <jelito@gmail.com>
2017-11-30 16:34:13 +0000

    backgrounds refactoring

--- js/script.js
+++ js/script.js
@@ -8,23 +8,22 @@ var bg2 = document.getElementById("bg2");
 var bgWrap = document.getElementById("bg-wrap");
 var bgWrap2 = document.getElementById("bg-wrap2");
 var prevBackground = -1;
-var prevBackground2 = -1;
 
 
 document.onload = (function() {
     // On Page load, we change to a random background image every 10 seconds.
-    newBackground();
+    newBackground(bg);
+    newBackground(bg2);
     setInterval(() => {
-        newBackground();
+        newBackground(bg);
     }, 21000);
+    setInterval(() => {
+        newBackground(bg2);
+    }, 11000);
 })();
 
 button1.addEventListener("click", () => {
     button1.classList.toggle('activebutton1');
-    // secButtons.classList.toggle('activesecButtons');    
-
-    // secButtons.classList.toggle('activesecButtons');
-
 
     if (button1.classList.contains('activebutton1')) {
         showSecButtons();
@@ -32,7 +31,6 @@ button1.addEventListener("click", () => {
         hideSecButtons();
     }
 });
-
 
 function showSecButtons() {
     button2.classList = ["slideIn"];
@@ -44,26 +42,18 @@ function hideSecButtons() {
     button2.classList = ["slideOut"];
 }
 
-function newBackground() {
+function newBackground(element) {
 
     var n;
     do {
         n = Math.floor(Math.random() * 21) + 1; // Random int between 1 and 21
-        n2 = Math.floor(Math.random() * 21) + 1; // Random int between 1 and 21
-    } while (n == prevBackground || n2 == prevBackground2 || n == n2);
+    } while (n == prevBackground);
     prevBackground = n;
-    prevBackground2 = n2;
-    console.log("New backgrounds:", n, n2);
 
-    bg.src = "";
-    bg2.src = "";
-    bg.classList = ["fadeOut"];
-    bg2.classList = ["fadeOut"];
-
+    element.src = "";
+    element.classList = ["fadeOut"];
     setTimeout(() => {
-        bg.src = "img/bg" + n + ".jpg";
-        bg2.src = "img/bg" + n2 + ".jpg";
-        bg.classList = ["fadeInOut"];
-        bg2.classList = ["fadeInOut2"];
+        element.src = "img/bg" + n + ".jpg";
+        element.classList = element.id == "bg" ? ["fadeInOut"] : ["fadeInOut2"];
     }, 1000);
 }
